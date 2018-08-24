37cb2d6fc9d24b31b1e77298c45af7cbaaacbb5d
abgeorge7 <abgeorge@umich.edu>
2018-08-24 11:06:04 -0500

    custom app.js page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Button from '@gen3/ui-component/dist/components/button';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div className='app'>
         <h1>Welcome to Gen3!</h1>
+        <Button />
       </div>
     );
   }
