bc2420bdea74351dc8d6d2f4c1124cac0331a379
abgeorge7 <abgeorge@umich.edu>
2018-08-24 11:59:09 -0500

    login button on homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Button from '@gen3/ui-component/dist/components/button';
+import Button from '@gen3/ui-component/dist/components/button/Button';
 import logo from './logo.svg';
 import './App.css';
 
@@ -8,7 +8,8 @@ class App extends Component {
     return (
       <div className='app'>
         <h1>Welcome to Gen3!</h1>
-        <Button />
+        <br />
+        <Button className='btn-primary-orange' label='Login' />
       </div>
     );
   }
