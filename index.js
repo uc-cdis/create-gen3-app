79c8aa81df76acfabbcad68d6229097c1db2a599
abgeorge7 <abgeorge@umich.edu>
2018-08-30 11:29:10 -0500

    alter way to find directory

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 #!/usr/bin/env node
 var shell = require('shelljs');
+var path = require('path');
 
 const newApp = process.argv.slice(2);
-const currentDir = process.cwd();
+const gen3Dir = path.join(__dirname);
+console.log('current directory:', gen3Dir);
+
+console.log('Creating a new Gen3 app using create-react-app...');
+console.log();
+
 shell.exec(`npm init react-app ${newApp}`);
 shell.cd(`${newApp}`);
+
+console.log();
+console.log('Installing Gen3 dependencies...')
+console.log();
+
 shell.exec("npm install --save '@gen3/ui-component'");
 shell.exec('npm install');
-shell.cp(`${currentDir}/src/App.js`, 'src/App.js');
-shell.cp(`${currentDir}/src/App.css`, 'src/App.css');
+shell.cp(`${gen3Dir}/src/App.js`, 'src/App.js');
+shell.cp(`${gen3Dir}/src/App.css`, 'src/App.css');
+
+console.log();
+console.log('Gen3 app successfully created!');
+console.log('Run the following commands to get started:');
+console.log(`cd ${newApp}`);
+console.log('npm start');
